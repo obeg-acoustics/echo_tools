95da03860816f48adb57b750a18e083addc50caf
Jerome Guiet <jguiet@jupyter.atmos.ucla.edu>
2021-06-21 12:38:38 -0700

    Update filter_echogram.m with new params and new RM filter, additional rmnan step

--- filter_echogram.m
+++ filter_echogram.m
@@ -17,14 +17,22 @@ clock
 % Motion correction TO TEST
 %[echogram] = motion_correction(echogram);
 
+% Few echogram corrections
+disp('Prepare echogram')
+% RM rows with NaN values
+[echogram] = rmnan(echogram);
+% Correct ranges
+for k = 1:5
+    echogram.pings(k).range = echogram.pings(k).range - (echogram.calParms(k).pulselength*(echogram.calParms(k).soundvelocity/4));
+end
 
 % Remove Impulsive Noise (IN)
-% INthreshold = 10;
-% INn = 1;
-% INmin = -80;
-disp('IN removal') 
-[echogram] = IN_filter(echogram, INthreshold, INn, INmin);
-
+% INthreshold = 6;
+% INsmooth    = 5;
+% INnmax      = 4; % (up to 4)
+% INminSv     = -170;
+disp('IN removal')
+[echogram] = IN_filter(echogram, INthreshold, INsmooth, INnmax, INminSv);
 
 %% Binning =========================
 % % We vertically bin this echogram chunk
@@ -40,29 +48,36 @@ disp('Bottom detect')
 
 % Remove Attenuation Signal (AS) ===
 % ASthreshold = 8;
-% ASn = 100; % In [30,300]
-% R1  = 300;
-% R2  = 400; 
+% ASn         = 300; % In [30,300]
+% R1          = 500;
+% R2          = 600;
 disp('AS removal')
 [echogram] = AS_filter(echogram, ASthreshold, ASn, R1, R2)
 
 % Remove Transient Noise (TN) ======
 % TNthreshold = 15;
-% TNn = 50;
-% TNm = 50; % ~10m
-% TNmin = -70;
-% mindepth = 300;
+% TNprctile   = 15;
+% TNsmooth    = 20;
+% TNn         = 50;
+% TNm         = 10;
+% TNminSv     = -150;
+% mindepth    = 250;
 disp('TN removal')
-[echogram] = TN_filter(echogram, TNthreshold, TNn, TNm, TNmin, mindepth)
+[echogram] = TN_filter(echogram, TNthreshold, TNsmooth, TNn, TNm, TNminSv, mindepth, TNprctile)
 
 % Background noise removal (BG) ====
-% BGn              = 40;
-% BGm              = 50;
+% BGn              = 10;
+% BGm              = 15;
 % thresholdSNR     = 10;
-% noisemax         = -125;
+% noisemax         = -100;
 disp('BG removal')
 [echogram] = BG_filter(echogram, BGn, BGm, noisemax, thresholdSNR);
 
+% Residual noise removal (RN) ====
+% RNthreshold = -50;
+% RNminSv     = -160;
+disp('RN removal')
+[echogram] = RN_filter(echogram, RNthreshold, RNminSv);
 
 % Bottom removal ===================
 %disp('Bottom removal')
@@ -75,6 +90,7 @@ disp('BG removal')
 [echogram] = vertical_binning(echogram, 1);
 % Horizontal binning
 [echogram] = horizontal_binning_time(echogram, 30);
+%[echogram] = horizontal_binning_distance(echogram, 1000);
 % Update distance for binned resolution
 [echogram] = distance_correct(echogram)
 
