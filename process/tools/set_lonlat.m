d95a799adb00d31d33c62eec3dcac05612d15d5a
Jerome Guiet <jguiet@poseidon.atmos.ucla.edu>
2021-08-25 16:17:19 -0700

    New script to set lon/lat for fixed platforms

---
+++ process/tools/set_lonlat.m
@@ -0,0 +1,26 @@
+function [echogram] = set_lonlat(echogram,LON,LAT)
+% This script set fixed longitude/latitude for fixed platforms
+
+for k = 1:length(echogram.pings)
+ 
+    if ~isfield(echogram.pings(k),'lon')
+	tmp = ones(size(echogram.pings(k).time));
+        echogram.pings(k).lon = LON*tmp;
+    end
+    if ~isfield(echogram.pings(k),'lat')
+	tmp = ones(size(echogram.pings(k).time));
+        echogram.pings(k).lat = LAT*tmp;
+    end
+
+end
+
+if ~isfield(echogram.gps,'lat')
+    tmp = ones(size(echogram.pings(1).time));
+    echogram.gps.lat = LAT*tmp; 
+end
+if ~isfield(echogram.gps,'lon') 
+    tmp = ones(size(echogram.pings(1).time));
+    echogram.gps.lon = LON*tmp;
+end
+
+return
